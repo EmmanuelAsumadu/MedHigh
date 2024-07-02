be458c9326118d7417b78479918ba79df76556a1
technocopia <technocopia@technocopia-ThinkPad-T431s>
2024-07-02 19:42:52 -0400

    Ignore the project files

--- MedHigh.groovy
+++ MedHigh.groovy
@@ -1,1 +1,16 @@
 // code here
+import eu.mihosoft.vrl.v3d.ChamferedCube
+int ox = 250
+int oy= 250
+int oz = 250
+int outCham = 15
+
+CSG outCasing = new ChamferedCube(
+	ox,// X 
+	oy, // Y
+	oz, // Z
+	 outCham// Chamfer length
+	).toCSG()
+	
+	
+	
