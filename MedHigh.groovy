e354307c22b22bf252a5786028023a6603134790
technocopia <technocopia@technocopia-ThinkPad-T431s>
2024-07-11 21:17:02 -0400

    MedHigh.ino

--- MedHigh.groovy
+++ MedHigh.groovy
@@ -1,23 +1,31 @@
 // code here
 import eu.mihosoft.vrl.v3d.ChamferedCube
 import eu.mihosoft.vrl.v3d.Cube
-int ox = 250;
-int oy= 250;
-int oz = 250;
+
+int ox = 250
+int oy = 250
+int oz = 250
 int outCham = 15
-int inx = 200 
+int inx = 200
 int iny = 200
 int inz = 200
+int compx = 100
+int compy = iny-(oy/iny)
+int compz = 80
+
 
 CSG outCasing = new ChamferedCube(
-	ox,// X 
-	oy, // Y
-	oz, // Z
-	 outCham// Chamfer length
-	).toCSG()
-CSG inCase = new Cube(inx,iny,inz).toCSG()
-return [outCasing,inCase]
+    ox, // X 
+    oy, // Y
+    oz, // Z
+    outCham // Chamfer length
+).toCSG().movez(oz / 2)
 
-	
-	
-	
+CSG inCase = new Cube(inx, iny, inz).toCSG().movez(inz / 2)
+CSG casing = outCasing.difference(inCase)
+CSG openCompartment  =  new Cube(compx,compy,compz).toCSG().movez(inx/3).movex(ox/2)
+CSG shell =casing.difference(openCompartment)
+
+
+
+return shell
